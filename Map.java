4f2d2df7c868dfb8c72113c30819950923813d70
aminouminou969-prog <aminouminou969@gmail.com>
2025-12-19 14:15:31 +0100

    Add methods to add delivery zones and no-fly zones in Map class

--- Map.java
+++ Map.java
@@ -4,5 +4,9 @@ import java.util.List;
 public class Map {
     private List<DeliveryZone> deliveryZones = new ArrayList<>();
     private List<NoFlyZone> noFlyZones = new ArrayList<>();
+
+    public void addDeliveryZone(DeliveryZone z){deliveryZones.add(z);}
+    public void addNoFlayZone(NoFlyZone z){noFlyZones.add(z);}
+    
     
 }
