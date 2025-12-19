84d44ad685563b088568465dbbdd69ff0bfd187a
aminouminou969-prog <aminouminou969@gmail.com>
2025-12-19 16:41:29 +0100

    Add base position and map attributes to ControlCenter class

--- ControlCenter.java
+++ ControlCenter.java
@@ -4,5 +4,8 @@ public class ControlCenter {
     private List<Order> pendigOrders=new ArrayList<>();
     private List<Order> processedOrders=new ArrayList<>();
     private java.util.Map<Drone, Order> activeDeliveries = new HashMap<>();
+
+    private Position base;
+    private Map map;
     
 }
