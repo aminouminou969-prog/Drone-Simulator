f0e714cb321f945348ad0d25f5da9be8925d9cc7
Hocine19-hanifi <hanifihocine618@gmail.com>
2025-12-19 11:53:09 +0100

    Implement StandardPackage class with weight and destination attributes

--- StandardPackage.java
+++ StandardPackage.java
@@ -1,2 +1,12 @@
 
+import java.util.Objects;
+
+public class StandardPackage implements Deliverable 
+    private double weight;
+    private Position destination;
+
+    public StandardPackage(double weight, Position destination) {
+        this.weight = weight;
+        this.destination = destination;
+    }
 
