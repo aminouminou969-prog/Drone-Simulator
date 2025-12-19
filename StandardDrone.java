73941aa8dcea6cc7d8442b10a06e7e5f75f95ad4
Hocine19-hanifi <hanifihocine618@gmail.com>
2025-12-19 16:42:39 +0100

    Refactor StandardDrone constructor and implement calculateConsumption method

--- StandardDrone.java
+++ StandardDrone.java
@@ -1,3 +1,8 @@
 public class StandardDrone extends Drone {
    public StandardDrone(Position position, double battery) {
-     
+     super(position, battery, "StandardDrone", 30.0, 1.0);
+    }
+    @Override
+     public double calculateConsumption(double distance) { return 3.0 * distance; }
+}  
+
