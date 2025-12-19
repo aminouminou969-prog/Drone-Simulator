b6683eafb7a8a4b60c1035e9bf961df1f9f72693
Hocine19-hanifi <hanifihocine618@gmail.com>
2025-12-19 17:08:22 +0100

    Refactor ExpressDrone class to extend Drone and implement energy consumption calculation

--- ExpressDrone.java
+++ ExpressDrone.java
@@ -1,3 +1,8 @@
-public class ExpressDrone {
+public class ExpressDrone extends Drone {
+ public ExpressDrone(Position position, double battery) {
+     super(position, battery, "ExpressDrone", 45.0, 1.0);
+    }
+     @Override
+       public double calculateConsumption(double distance) { return 4.0 * distance; }    
     
 }
