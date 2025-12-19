5e867ab0ed7cea6e1e074a7d01da23ce3a6fafb3
aminouminou969-prog <aminouminou969@gmail.com>
2025-12-19 14:08:41 +0100

    Refactor Map class to initialize deliveryZones and noFlyZones lists

--- Map.java
+++ Map.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Map {
-    
+    private List<DeliveryZone> deliveryZones = new ArrayList<>();
+    private List<NoFlyZone> noFlyZones = new ArrayList<>();
     
 }
