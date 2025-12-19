2178311aa02f738186ec33432dff74924428b87e
Hocine19-hanifi <hanifihocine618@gmail.com>
2025-12-19 19:01:03 +0100

    Fix method name for adding no-fly zone in the map

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         Map map = new Map();
-        map.addNoFlyZone(new NoFlyZone(new Position(3, 3), 2.5));
+        map.addDeliveryZone(new NoFlyZone(new Position(3, 3), 2.5));
 
         Position base = new Position(0, 0);
         ControlCenter center = new ControlCenter(base, map);
@@ -29,7 +29,7 @@ public class Main {
         System.out.println("Energy StandardDrone: " + String.format("%.2f", ControlCenter.energyConsumedStandard) + " %");
         System.out.println("Energy ExpressDrone: " + String.format("%.2f", ControlCenter.energyConsumedExpress) + " %");
         System.out.println("Energy HeavyDrone: " + String.format("%.2f", ControlCenter.energyConsumedHeavy) + " %");
-
+        
         System.out.println("\n===== DRONES =====");
         for (Drone d : center.getFleet()) System.out.println(d);
 
