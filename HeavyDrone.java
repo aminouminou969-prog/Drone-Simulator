2fdf3878c01c4b29f14b8582e43351bdcaf22607
Hocine19-hanifi <hanifihocine618@gmail.com>
2025-12-19 17:21:16 +0100

    Implement HeavyDrone class extending Drone with consumption calculation and speed adjustment

--- HeavyDrone.java
+++ HeavyDrone.java
@@ -1,3 +1,15 @@
-public class HeavyDrone {
+public class HeavyDrone extends Drone {
+ public HeavyDrone(Position position, double battery) {
+    super(position, battery, "HeavyDrone", 20.0, 3.0);
+    }
+        @Override
+          public double calculateConsumption(double distance) { return 5.0 * distance; }
+            @Override
+             protected double getEffectiveSpeedKmh() {
+                 if (getBattery() < 20.0) return 16.0;
+        return getSpeed();
+             }  
+
+           
     
 }
