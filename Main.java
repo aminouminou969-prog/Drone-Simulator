c3739fda9190b7aa9402d566010ab1dac9266106
Hocine19-hanifi <hanifihocine618@gmail.com>
2025-12-19 17:34:32 +0100

    Add no-fly zone and initialize control center in main method

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
    public static void main(String[] args) {
      Map map = new Map();
-       
+     map.addNoFlyZone(new NoFlyZone(new Position(3, 3), 2.5));
+       Position base = new Position(0, 0);
+        ControlCenter center = new ControlCenter(base, map); 
 }
