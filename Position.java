21512b86eaa70ca1486c688d93aad1d19714efad
aminouminou969-prog <aminouminou969@gmail.com>
2025-12-19 08:32:40 +0100

    Remove Position class implementation

--- Position.java
+++ Position.java
@@ -1,14 +1,2 @@
 import java.util.Objects;
 
-public class Position {
-    
-    private double x;
-    private double y;
-
-     public Position(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-
-}
