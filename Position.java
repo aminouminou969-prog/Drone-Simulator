f97a78063ba10df23d536561a57f1b72325621b6
aminouminou969-prog <aminouminou969@gmail.com>
2025-12-19 08:41:27 +0100

    Add distanceTo method to Position class for calculating distance between two positions

--- Position.java
+++ Position.java
@@ -4,5 +4,11 @@ public class Position{
     private double x;
     private double y;
 
-    public Position(double x, double y){this.x = x; this.y = y;} 
+    public Position(double x, double y){this.x = x; this.y = y;}
+    
+    public double distanceTo(Position other){
+        double dx = this.x - other.x;
+        double dy = this.y - other.y;
+        return Math.sqrt(dx*dx+dy*dy);
+    }
 }
