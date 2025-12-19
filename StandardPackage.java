13d4ca6a24d78bda84bdfe357c9e9f582e61270c
Hocine19-hanifi <hanifihocine618@gmail.com>
2025-12-19 12:07:59 +0100

    Refactor StandardPackage class to improve structure and readability

--- StandardPackage.java
+++ StandardPackage.java
@@ -1,1 +1,16 @@
 
+import java.util.Objects;
+
+public class StandardPackage implements Deliverable {
+    private double weight;
+    private Position destination;
+
+    public StandardPackage(double weight, Position destination) {
+        this.weight = weight;
+        this.destination = destination;
+    }
+
+  @Override
+    public double getWeight() {
+        return weight;
+    }
