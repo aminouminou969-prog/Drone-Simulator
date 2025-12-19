4e5ae9faeecf818949f2180d0dff6b7c453410bf
aminouminou969-prog <aminouminou969@gmail.com>
2025-12-19 20:38:50 +0100

    Implement drone movement and delivery handling per minute

--- Simulator.java
+++ Simulator.java
@@ -11,4 +11,47 @@ public class Simulator {
         }
     }
     
+    private void moveDronesOneMinute(){
+        java.util.Map<Drone, Order> active = new HashMap<>(controlCenter.getActiveDeliveries());
+
+        for(java.util.Map.Entry<Drone,Order> e : active.entrySet()){
+            Drone d = e.getKey();
+            Order o = e.getValue();
+            if("IN DELIVERY".equals(d.getStatus())){
+                Position dest = o.getDeliverable().getDestination();
+                double beforeBattery = d.getBattery();
+                double beforeDist = d.getTotalDistance();
+
+                d.flyTo(dest);
+                ControlCenter.totalDistance += (d.getTotalDistance() - beforeDist);
+                ControlCenter.addEnergyConsumed(d, beforeBattery - d.getBattery());
+
+                if(d.getBattery() <= 0 && !dest.equals(d.getPosition())){
+                    controlCenter.markFailed(d);
+                    d.setStatus("AVAILABLE");
+                    controlCenter.finishDelivery(d);
+                } else if (dest.equals(d.getPosition())){
+                    controlCenter.markDelivered(d);
+                    d.setStatus("RETURN TO BASE");
+
+                }
+            }else if ("RETURN TO BASE".equals(d.getStatus())){
+                Position base = controlCenter.getBase();
+                double beforeBattery = d.getBattery();
+                double beforeDist = d.getTotalDistance();
+
+                d.flyTo(base);
+                ControlCenter.totalDistance +=(d.getTotalDistance() - beforeDist);
+                ControlCenter.addEnergyConsumed(d, beforeBattery - d.getBattery());
+
+                if (base.equals(d.getPosition())){
+                    d.setStatus("AVAILABLE");
+                    controlCenter.finishDelivery(d);
+                }
+            }
+
+
+            
+        }
+    }
 }
