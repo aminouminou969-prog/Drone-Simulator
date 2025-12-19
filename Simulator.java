34ffaa4e821007b545da8462d65958fc7cbde99e
aminouminou969-prog <aminouminou969@gmail.com>
2025-12-19 20:07:58 +0100

    Add daily simulation logic to Simulator

--- Simulator.java
+++ Simulator.java
@@ -1,4 +1,14 @@
 import java.util.*;
 public class Simulator {
+    private ControlCenter controlCenter;
+
+    public void simulateDay(ControlCenter controlCenter){ this.controlCenter = controlCenter; }
+
+    public void simulaterDay(){
+        for(int minute = 1; minute <= 480; minute++){
+            controlCenter.processOneMinute();
+            moveDronesOneMinute();
+        }
+    }
     
 }
